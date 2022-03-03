401d7948f8ad23a146a302f38063e6679600680d
ksherm45 <77851772+ksherm45@users.noreply.github.com>
2022-03-03 17:48:51 +0100

    Components and Links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Pods from './components/Pods';
+import Audio from './components/Audio';
+import Video from './components/Video';
+import Library from './components/Library';
+import Homepage from './components/Homepage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar/>
+
+    <Routes>
+    <Route path="/Homepage" element={<Homepage/>} />
+    <Route path="/Audio" element={<Audio/>} />
+    <Route path="/Video" element={<Video/>} />
+    <Route path="/Pods" element={<Pods/>} />
+    <Route path="/Library" element={<Library/> } />
+
+
+    </Routes>
+
     </div>
   );
 }
---
+++ src/components/Pods.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Pods() {
+  return (
+    <div>Pods</div>
+  )
+}
+
+export default Pods
